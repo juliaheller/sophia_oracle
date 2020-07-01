089075543261135fe407a9b0b0761a0fa351b83a
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-07-01 14:18:31 +0200

    Updates

---
+++ precache-manifest.71f9ff029237fcaad435c8b07c8b7766.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7286b77111e8f6fd222f23abf456c0f",
+    "url": "/sophia_oracle/index.html"
+  },
+  {
+    "revision": "368439b24c5cee7daafe",
+    "url": "/sophia_oracle/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "454f19cb5208f635d3f5",
+    "url": "/sophia_oracle/static/js/2.80308171.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sophia_oracle/static/js/2.80308171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368439b24c5cee7daafe",
+    "url": "/sophia_oracle/static/js/main.486f7472.chunk.js"
+  },
+  {
+    "revision": "123dc7fab4c4ddd01bfd",
+    "url": "/sophia_oracle/static/js/runtime-main.7d8d05b3.js"
+  },
+  {
+    "revision": "dedf103d8499e999f9a7b397c792341b",
+    "url": "/sophia_oracle/static/media/clouds.dedf103d.gif"
+  },
+  {
+    "revision": "869e64f3a28d364a13b3a92063878615",
+    "url": "/sophia_oracle/static/media/goddess_tempel.869e64f3.png"
+  },
+  {
+    "revision": "46ba6886d9881bf3358acb3e12149b0b",
+    "url": "/sophia_oracle/static/media/parchment.46ba6886.png"
+  }
+]);
